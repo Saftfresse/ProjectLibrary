737f41f1b33c5b95d612e4caf56682b5eb9f41ba
Sven Greinert <development@sgr.one>
2023-03-05 20:55:56 +0100

    Expanded the Tools from local changes

---
+++ ProjectClassLib/ProjectClassLib/Classes/Margin.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjectClassLib.Classes
+{
+	public class Margin
+	{
+		int top, right, bottom, left;
+
+		public int Top { get => top; set => top = value; }
+		public int Right { get => right; set => right = value; }
+		public int Bottom { get => bottom; set => bottom = value; }
+		public int Left { get => left; set => left = value; }
+		public int HorizontalMargins { get => left + right; }
+		public int VerticalMargins { get => top + bottom; }
+
+		public Margin()
+		{
+
+		}
+		public Margin(int _all)
+		{
+			top = _all;
+			right = _all;
+			bottom = _all;
+			left = _all;
+		}
+
+		public Margin(int _top, int _right, int _bottom, int _left)
+		{
+			top = _top;
+			right = _right;
+			bottom = _bottom;
+			left = _left;
+		}
+	}
+}
--- ProjectClassLib/ProjectClassLib/Tools.cs
+++ ProjectClassLib/ProjectClassLib/Tools.cs
@@ -1,12 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Drawing.Imaging;
 using System.IO;
 using System.Linq;
 using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Xml;
+using System.Xml.Serialization;
 
 namespace ProjectClassLib
 {
@@ -136,5 +140,361 @@ namespace ProjectClassLib
                 return BitConverter.ToString(hash).Replace("-", "").ToLowerInvariant();
             }
         }
+
+        /// <summary>
+        /// Gibt einen Punkt auf einem Kreis zuzrück anhand einem gegebenen Winkel
+        /// </summary>
+        /// <param name="radius">Der Radius des Kreises</param>
+        /// <param name="angleInDegrees">Der Winkel des gesuchten Punktes</param>
+        /// <param name="origin">Das Zentrum des Kreises</param>
+        /// <returns>Den Punkt auf dem Kreis</returns>
+        public static PointF PointOnCircle(float radius, float angleInDegrees, PointF origin)
+        {
+            // Convert from degrees to radians via multiplication by PI/180        
+            float x = (float)(radius * Math.Cos(angleInDegrees * Math.PI / 180F)) + origin.X;
+            float y = (float)(radius * Math.Sin(angleInDegrees * Math.PI / 180F)) + origin.Y;
+
+            return new PointF(x, y);
+        }
+
+        /// <summary>
+        /// Prüfe ob ein gegebener Punkt in einer Ellipse enthalten ist
+        /// </summary>
+        /// <param name="ellipse">Die vorhandene Ellipse</param>
+        /// <param name="location">Der Punkt der getestet werden soll</param>
+        /// <returns></returns>
+        public static bool EllipseContains(Rectangle ellipse, Point location)
+        {
+            GraphicsPath myPath = new GraphicsPath();
+            myPath.AddEllipse(ellipse);
+            return myPath.IsVisible(location);
+        }
+
+        public static bool IsPointInPolygon(PointF p, PointF[] polygon)
+        {
+            double minX = polygon[0].X;
+            double maxX = polygon[0].X;
+            double minY = polygon[0].Y;
+            double maxY = polygon[0].Y;
+            for (int i = 1; i < polygon.Length; i++)
+            {
+                PointF q = polygon[i];
+                minX = Math.Min(q.X, minX);
+                maxX = Math.Max(q.X, maxX);
+                minY = Math.Min(q.Y, minY);
+                maxY = Math.Max(q.Y, maxY);
+            }
+
+            if (p.X < minX || p.X > maxX || p.Y < minY || p.Y > maxY)
+            {
+                return false;
+            }
+
+            bool inside = false;
+            for (int i = 0, j = polygon.Length - 1; i < polygon.Length; j = i++)
+            {
+                if ((polygon[i].Y > p.Y) != (polygon[j].Y > p.Y) &&
+                     p.X < (polygon[j].X - polygon[i].X) * (p.Y - polygon[i].Y) / (polygon[j].Y - polygon[i].Y) + polygon[i].X)
+                {
+                    inside = !inside;
+                }
+            }
+
+            return inside;
+        }
+
+        /// <summary>
+        /// Returns a Rectangle with a border radius
+        /// </summary>
+        /// <param name="bounds"></param>
+        /// <param name="radius"></param>
+        /// <returns></returns>
+        public static GraphicsPath RoundedRect(Rectangle bounds, int radius)
+        {
+            int diameter = radius * 2;
+            Size size = new Size(diameter, diameter);
+            Rectangle arc = new Rectangle(bounds.Location, size);
+            GraphicsPath path = new GraphicsPath();
+
+            if (radius == 0)
+            {
+                path.AddRectangle(bounds);
+                return path;
+            }
+
+            // top left arc  
+            path.AddArc(arc, 180, 90);
+
+            // top right arc  
+            arc.X = bounds.Right - diameter;
+            path.AddArc(arc, 270, 90);
+
+            // bottom right arc  
+            arc.Y = bounds.Bottom - diameter;
+            path.AddArc(arc, 0, 90);
+
+            // bottom left arc 
+            arc.X = bounds.Left;
+            path.AddArc(arc, 90, 90);
+
+            path.CloseFigure();
+            return path;
+        }
+
+        /// <summary>
+        /// Draws a rounded rectangle on the specified graphics
+        /// </summary>
+        /// <param name="graphics"></param>
+        /// <param name="pen"></param>
+        /// <param name="bounds"></param>
+        /// <param name="cornerRadius"></param>
+        public static void DrawRoundedRectangle(Graphics graphics, Pen pen, Rectangle bounds, int cornerRadius, SmoothingMode _smoothing = SmoothingMode.AntiAlias)
+        {
+            if (graphics == null)
+                throw new ArgumentNullException("graphics");
+            if (pen == null)
+                throw new ArgumentNullException("pen");
+
+            using (GraphicsPath path = RoundedRect(bounds, cornerRadius))
+            {
+                graphics.SmoothingMode = _smoothing;
+                graphics.DrawPath(pen, path);
+            }
+        }
+
+        /// <summary>
+        /// Fills a rounded rectangle on the specified graphics
+        /// </summary>
+        /// <param name="graphics"></param>
+        /// <param name="brush"></param>
+        /// <param name="bounds"></param>
+        /// <param name="cornerRadius"></param>
+        public static void FillRoundedRectangle(Graphics graphics, Brush brush, Rectangle bounds, int cornerRadius, SmoothingMode _smoothing = SmoothingMode.AntiAlias)
+        {
+            if (graphics == null)
+                throw new ArgumentNullException("graphics");
+            if (brush == null)
+                throw new ArgumentNullException("brush");
+
+            using (GraphicsPath path = RoundedRect(bounds, cornerRadius))
+            {
+                graphics.SmoothingMode = _smoothing;
+                graphics.FillPath(brush, path);
+            }
+        }
+
+
+        /// <summary>
+        /// Returns a Rectangle with a border radius
+        /// </summary>
+        /// <param name="bounds"></param>
+        /// <param name="radius"></param>
+        /// <returns></returns>
+        public static GraphicsPath RoundedRectF(RectangleF bounds, float radius)
+        {
+            float diameter = radius * 2;
+            SizeF size = new SizeF(diameter, diameter);
+            RectangleF arc = new RectangleF(bounds.Location, size);
+            GraphicsPath path = new GraphicsPath();
+
+            if (radius == 0)
+            {
+                path.AddRectangle(bounds);
+                return path;
+            }
+
+            // top left arc  
+            path.AddArc(arc, 180, 90);
+
+            // top right arc  
+            arc.X = bounds.Right - diameter;
+            path.AddArc(arc, 270, 90);
+
+            // bottom right arc  
+            arc.Y = bounds.Bottom - diameter;
+            path.AddArc(arc, 0, 90);
+
+            // bottom left arc 
+            arc.X = bounds.Left;
+            path.AddArc(arc, 90, 90);
+
+            path.CloseFigure();
+            return path;
+        }
+
+        /// <summary>
+        /// Draws a rounded rectangle on the specified graphics
+        /// </summary>
+        /// <param name="graphics"></param>
+        /// <param name="pen"></param>
+        /// <param name="bounds"></param>
+        /// <param name="cornerRadius"></param>
+        public static void DrawRoundedRectangleF(Graphics graphics, Pen pen, RectangleF bounds, float cornerRadius, SmoothingMode _smoothing = SmoothingMode.AntiAlias)
+        {
+            if (graphics == null)
+                throw new ArgumentNullException("graphics");
+            if (pen == null)
+                throw new ArgumentNullException("pen");
+
+            using (GraphicsPath path = RoundedRectF(bounds, cornerRadius))
+            {
+                graphics.SmoothingMode = _smoothing;
+                graphics.DrawPath(pen, path);
+            }
+        }
+
+        /// <summary>
+        /// Fills a rounded rectangle on the specified graphics
+        /// </summary>
+        /// <param name="graphics"></param>
+        /// <param name="brush"></param>
+        /// <param name="bounds"></param>
+        /// <param name="cornerRadius"></param>
+        public static void FillRoundedRectangleF(Graphics graphics, Brush brush, RectangleF bounds, float cornerRadius, SmoothingMode _smoothing = SmoothingMode.AntiAlias)
+        {
+            if (graphics == null)
+                throw new ArgumentNullException("graphics");
+            if (brush == null)
+                throw new ArgumentNullException("brush");
+
+            using (GraphicsPath path = RoundedRectF(bounds, cornerRadius))
+            {
+                graphics.SmoothingMode = _smoothing;
+                graphics.FillPath(brush, path);
+            }
+        }
+
+        /// <summary>
+        /// Returns a Image with rounded corners
+        /// </summary>
+        /// <param name="StartImage"></param>
+        /// <param name="CornerRadius"></param>
+        /// <param name="BackgroundColor"></param>
+        /// <returns></returns>
+        public static Image RoundCorners(Image StartImage, int CornerRadius, Color BackgroundColor)
+        {
+            CornerRadius *= 2;
+            Bitmap RoundedImage = new Bitmap(StartImage.Width, StartImage.Height);
+            using (Graphics g = Graphics.FromImage(RoundedImage))
+            {
+                g.Clear(BackgroundColor);
+                g.SmoothingMode = SmoothingMode.AntiAlias;
+                Brush brush = new TextureBrush(StartImage);
+                GraphicsPath gp = new GraphicsPath();
+                gp.AddArc(0, 0, CornerRadius, CornerRadius, 180, 90);
+                gp.AddArc(0 + RoundedImage.Width - CornerRadius, 0, CornerRadius, CornerRadius, 270, 90);
+                gp.AddArc(0 + RoundedImage.Width - CornerRadius, 0 + RoundedImage.Height - CornerRadius, CornerRadius, CornerRadius, 0, 90);
+                gp.AddArc(0, 0 + RoundedImage.Height - CornerRadius, CornerRadius, CornerRadius, 90, 90);
+                g.FillPath(brush, gp);
+                return RoundedImage;
+            }
+        }
+
+        /// <summary>
+        /// Return a Byte Array from a Bitmap
+        /// </summary>
+        /// <param name="img"></param>
+        /// <returns></returns>
+        public static byte[] ImageToByte2(Image img)
+        {
+            using (var stream = new MemoryStream())
+            {
+                img.Save(stream, System.Drawing.Imaging.ImageFormat.Png);
+                return stream.ToArray();
+            }
+        }
+
+        public static void SerializeObject<T>(T serializableObject, string fileName)
+        {
+            if (serializableObject == null) { return; }
+
+            try
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                XmlSerializer serializer = new XmlSerializer(serializableObject.GetType());
+                using (MemoryStream stream = new MemoryStream())
+                {
+                    serializer.Serialize(stream, serializableObject);
+                    stream.Position = 0;
+                    xmlDocument.Load(stream);
+                    xmlDocument.Save(fileName);
+                }
+            }
+            catch (Exception ex)
+            {
+                //Log exception here
+            }
+        }
+
+        /// <summary>
+        /// Deserializes an xml file into an object list
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="fileName"></param>
+        /// <returns></returns>
+        public static T DeSerializeObject<T>(string fileName)
+        {
+            if (string.IsNullOrEmpty(fileName)) { return default(T); }
+
+            T objectOut = default(T);
+
+            try
+            {
+                XmlDocument xmlDocument = new XmlDocument();
+                xmlDocument.Load(fileName);
+                string xmlString = xmlDocument.OuterXml;
+
+                using (StringReader read = new StringReader(xmlString))
+                {
+                    Type outType = typeof(T);
+
+                    XmlSerializer serializer = new XmlSerializer(outType);
+                    using (XmlReader reader = new XmlTextReader(read))
+                    {
+                        objectOut = (T)serializer.Deserialize(reader);
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                //Log exception here
+            }
+
+            return objectOut;
+        }
+
+        /// <summary>
+        /// Recursively Deletes a Directory with all of its Sub-Directories an Files
+        /// </summary>
+        /// <param name="path"></param>
+        public static void DeleteDirectory(string path)
+        {
+            foreach (string directory in Directory.GetDirectories(path))
+            {
+                DeleteDirectory(directory);
+            }
+
+            try
+            {
+                Directory.Delete(path, true);
+            }
+            catch (IOException)
+            {
+                Directory.Delete(path, true);
+            }
+            catch (UnauthorizedAccessException)
+            {
+                Directory.Delete(path, true);
+            }
+        }
+
+        public static PointF CenterStringPoint(string _string, Font _font, RectangleF _bounds)
+		{
+            SizeF strSize = TextRenderer.MeasureText(_string, _font);
+            PointF pos = new PointF(_bounds.Left + _bounds.Width / 2 - strSize.Width / 2, _bounds.Top + _bounds.Height / 2 - strSize.Height / 2);
+
+
+            return pos;
+		}
     }
 }
